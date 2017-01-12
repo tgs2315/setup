bc2758f8977de0e68bb9936e58ed8db43ce42f35
Tom Schuhmann <tgs2315@gmail.com>
2017-01-12 02:21:12 -0500

    Updated setup.sh to work on Ubuntu 16.04 LTS EC2 instance

--- setup.sh
+++ setup.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# Simple setup.sh for configuring Ubuntu 16.04 LTS EC2 instance
 # for headless setup. 
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -23,9 +22,7 @@ sudo apt-get install -y rlwrap
 
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+sudo apt-get install -y emacs24
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
